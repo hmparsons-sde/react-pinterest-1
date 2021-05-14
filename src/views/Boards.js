aa7f2213e1f350f73e08d8f9d72e5a52b70c4019
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-05-13 19:35:54 -0500

    board form now works with a button

--- src/views/Boards.js
+++ src/views/Boards.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'reactstrap';
 import BoardCard from '../components/Cards/BoardCard';
 import BoardForm from '../components/Forms/BoardForm';
 
 function BoardsView({ user, boards, setBoards }) {
+  const [showButton, setShowButton] = useState(false);
+  const handleClick = () => {
+    setShowButton((prevState) => !prevState);
+  };
+
   return (
     <>
+      <section className="header">
+       { !showButton
+         ? <Button className="m-3" color='danger' onClick={handleClick}>Add Board</Button>
+         : <div>
+            <Button className="m-3" color='secondary' onClick={handleClick}>Close</Button>
+            <BoardForm className="justify-content-center mt-3" setBoards={setBoards} user={user}/>
+          </div>
+        }
+      </section>
+
       <div className="card-container" id="board-cards">
         {boards.map((boardInfo) => (
           <BoardCard
@@ -15,17 +31,11 @@ function BoardsView({ user, boards, setBoards }) {
             title={boardInfo.title}
             user={user}
             setBoards={setBoards}
-            // {...boards}
           />
         ))}
       </div>
       <div>
       <h1>Boards</h1>
-      <BoardForm
-        user={user}
-        formTitle='Add a Board'
-        setBoards={setBoards}
-      />
     </div>
     </>
   );
